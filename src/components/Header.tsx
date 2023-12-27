f37f24fd24fb878d7fa868fbffa6ed431b2627c3
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2023-12-27 11:29:07 -0500

    add: routing fix: header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -88,7 +88,7 @@ import {
     ));
   
     return (
-      <Box pb={120}>
+      <Box >
         <header className={classes.header}>
           <Group justify="space-between" h="100%">
             <MantineLogo size={30} />
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,34 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./views/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/about",
+        element: <h1>About</h1>,
+      },
+      {
+        path: "/contact",
+        element: <h1>Contact</h1>,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider>
-      <App />
+      <RouterProvider router={router}/>
     </MantineProvider>
   </React.StrictMode>
 );
