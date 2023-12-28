71a81a6705a2e2118a313c588fe753939c53373a
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2023-12-28 15:35:29 -0500

    chore: fix header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-
 import {
   HoverCard,
   Group,
@@ -17,9 +16,9 @@ import {
   ScrollArea,
   rem,
   useMantineTheme,
-} from '@mantine/core';
-import { MantineLogo } from '@mantinex/mantine-logo';
-import { useDisclosure } from '@mantine/hooks';
+} from "@mantine/core";
+import { MantineLogo } from "@mantinex/mantine-logo";
+import { useDisclosure } from "@mantine/hooks";
 import {
   IconStatusChange,
   IconThermometer,
@@ -28,46 +27,48 @@ import {
   IconArrowDownBar,
   IconMoodSick,
   IconChevronDown,
+} from "@tabler/icons-react";
 
-} from '@tabler/icons-react';
-
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
 
 const mockdata = [
   {
     icon: IconThermometer,
-    title: 'Dolor de cabeza',
-    description: 'Sensación dolorosa en cualquier parte de la cabeza, variando en intensidad.'
+    title: "Dolor de cabeza",
+    description:
+      "Sensación dolorosa en cualquier parte de la cabeza, variando en intensidad.",
   },
   {
     icon: IconMoodSick,
-    title: 'Fatiga',
-    description: ' Cansancio extremo y falta de energía persistente',
+    title: "Fatiga",
+    description: " Cansancio extremo y falta de energía persistente",
   },
   {
     icon: IconCircuitSwitchClosed,
-    title: 'Perdida de cabello',
-    description: 'Reducción notable de cabello en el cuero cabelludo.',
+    title: "Perdida de cabello",
+    description: "Reducción notable de cabello en el cuero cabelludo.",
   },
   {
     icon: IconArrowDownBar,
-    title: 'Bajo deseo sexual',
-    description: 'Disminución del interés en la actividad sexual.',
+    title: "Bajo deseo sexual",
+    description: "Disminución del interés en la actividad sexual.",
   },
   {
     icon: IconHazeMoon,
-    title: 'Sofocos / Sudores nocturnos',
-    description: 'Calor repentino y transpiración excesiva durante la noche',
+    title: "Sofocos / Sudores nocturnos",
+    description: "Calor repentino y transpiración excesiva durante la noche",
   },
   {
     icon: IconStatusChange,
-    title: 'Cambios de Humos /Ansiedad  / Depresion',
-    description: 'Alteraciones del estado de ánimo, inquietud y tristeza profunda.',
+    title: "Cambios de Humos /Ansiedad  / Depresion",
+    description:
+      "Alteraciones del estado de ánimo, inquietud y tristeza profunda.",
   },
 ];
 
 export function Header() {
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
   const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
   const theme = useMantineTheme();
 
@@ -75,7 +76,10 @@ export function Header() {
     <UnstyledButton className={classes.subLink} key={item.title}>
       <Group wrap="nowrap" align="flex-start">
         <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
+          <item.icon
+            style={{ width: rem(22), height: rem(22) }}
+            color={theme.colors.blue[6]}
+          />
         </ThemeIcon>
         <div>
           <Text size="sm" fw={500}>
@@ -90,7 +94,7 @@ export function Header() {
   ));
 
   return (
-    <Box>
+    <>
       <header className={classes.header}>
         <Group justify="space-between" h="100%">
           <MantineLogo size={30} />
@@ -98,7 +102,13 @@ export function Header() {
             <a href="/" className={classes.link}>
               Home
             </a>
-            <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
+            <HoverCard
+              width={600}
+              position="bottom"
+              radius="md"
+              shadow="md"
+              withinPortal
+            >
               <HoverCard.Target>
                 <a href="#" className={classes.link}>
                   <Center inline>
@@ -113,7 +123,7 @@ export function Header() {
                 </a>
               </HoverCard.Target>
 
-              <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
+              <HoverCard.Dropdown style={{ overflow: "hidden" }}>
                 <Group justify="space-between" px="md">
                   <Text fw={500}>Sintomas</Text>
                   <Anchor href="#" fz="xs">
@@ -134,7 +144,8 @@ export function Header() {
                         Realiza el examen
                       </Text>
                       <Text size="xs" c="dimmed">
-                        Realiza nuestro test para conocer como puedes mejorar tu vida
+                        Realiza nuestro test para conocer como puedes mejorar tu
+                        vida
                       </Text>
                     </div>
                     <Button variant="default">Empecemos</Button>
@@ -155,7 +166,11 @@ export function Header() {
             <Button>Sign up</Button>
           </Group>
 
-          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            hiddenFrom="sm"
+          />
         </Group>
       </header>
 
@@ -201,8 +216,8 @@ export function Header() {
           </Group>
         </ScrollArea>
       </Drawer>
-    </Box>
+    </>
   );
 }
 
-export default Header
+export default Header;
