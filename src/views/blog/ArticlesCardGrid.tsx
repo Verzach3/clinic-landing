56e2ceeb8e5135a52971cd46570a4916c204a105
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-01-05 11:29:54 -0500

    chore: backup changes

--- src/views/blog/ArticlesCardGrid.tsx
+++ src/views/blog/ArticlesCardGrid.tsx
@@ -1,26 +1,16 @@
-import { SimpleGrid, Card, Image, Text, Container, AspectRatio } from '@mantine/core';
-import classes from './ArticlesCardsGrid.module.css';
-import { BlogPost } from '../../types/blogPost';
-
+import { SimpleGrid, Container } from "@mantine/core";
+import { BlogPost } from "../../types/blogPost";
+import { ArticleCard } from "./ArticleCard";
+import { nanoid } from "nanoid";
 
 export function ArticlesCardsGrid({ items }: { items: BlogPost[] }) {
-    const cards = items.map((article) => (
-        <Card key={article.title} p="md" radius="md" component="a" href="#" className={classes.card}>
-            <AspectRatio ratio={1920 / 1080}>
-                <Image fit='fill' src={`https://directus.caprover.thepanas.me/assets/${article.image}`} />
-            </AspectRatio>
-            <Text c="dimmed" size="xs" tt="uppercase" fw={700} mt="md">
-                {(new Date(article.date_updated ?? article.date_created)).getDate()}
-            </Text>
-            <Text className={classes.title} mt={5}>
-                {article.title}
-            </Text>
-        </Card>
-    ));
+  const cards = items.map((article) => (
+    <ArticleCard key={nanoid()} post={article} />
+  ));
 
-    return (
-        <Container py="xl">
-            <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>
-        </Container>
-    );
+  return (
+    <Container py="xl">
+      <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>
+    </Container>
+  );
 }
